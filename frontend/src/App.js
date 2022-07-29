5083689bae5aa5529da9b65d2d015e561bf042b2
nirajgolhar <nsgolhar@gmail.com>
2022-07-29 14:50:07 +0530

    google font issue in idex.html file solved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,18 @@ import Home from './pages/Home'
 import Navbar from './components/Navbar'
 
 function App() {
-
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route 
-              path="/" 
-              element={<Home />} 
-            />
+            <Route path="/" element={<Home />} />
           </Routes>
         </div>
       </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
-
+export default App
